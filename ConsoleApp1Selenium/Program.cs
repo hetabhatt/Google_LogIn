bd370f30482a9b730ad93528bcecad70569d267b
heta bhatt <heta.bhatt09@gmail.com>
2021-04-29 17:49:55 +1200

    Add project files.

---
+++ ConsoleApp1Selenium/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Heta");
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Learn Automation");
+            driver.Quit();
+        }
+    }
+}
